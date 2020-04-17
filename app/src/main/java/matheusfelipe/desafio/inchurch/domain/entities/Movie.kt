20e824f9cf90797f60689ebf107495662bbf5a9e
Felcks <matheusfelipecorreaalves@gmail.com>
2020-04-17 18:20:23 -0300

    Primero commit no repositório forkado

--- app/src/main/java/matheusfelipe/desafio/inchurch/domain/entities/Movie.kt
+++ app/src/main/java/matheusfelipe/desafio/inchurch/domain/entities/Movie.kt
@@ -2,21 +2,23 @@ package matheusfelipe.desafio.inchurch.domain.entities
 
 import java.time.ZonedDateTime
 
-data class Movie(val id: Int,
-                 val originalTitle: String,
-                 val originalLanguage: String,
-                 val title: String,
-                 val posterPath: String,
-                 val isAdult: Boolean,
-                 val overview: String,
-                 val releaseDate: ZonedDateTime,
-                 val popularity: Double,
-                 val voteCount: Int,
-                 val video: Boolean,
-                 val voteAverage: Double,
-                 var genreIds: List<Int>){
+data class Movie(
+    val id: Int,
+    val originalTitle: String,
+    val originalLanguage: String,
+    val title: String,
+    val posterPath: String,
+    val isAdult: Boolean,
+    val overview: String,
+    val releaseDate: ZonedDateTime,
+    val popularity: Double,
+    val voteCount: Int,
+    val video: Boolean,
+    val voteAverage: Double,
+    var genreIds: List<Int>
+) {
 
-    fun getGenres(allGenres: List<Genre>): List<Genre>{
+    fun getGenres(allGenres: List<Genre>): List<Genre> {
         return allGenres.filter {
             genreIds.contains(it.id)
         }
