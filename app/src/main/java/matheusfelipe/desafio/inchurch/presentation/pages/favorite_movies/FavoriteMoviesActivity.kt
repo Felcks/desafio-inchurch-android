763f061d39c1c424d04fc7f851bdf3f485f64739
Felcks <matheusfelipecorreaalves@gmail.com>
2020-04-18 17:33:08 -0300

    FavoriteMoviesActivity 02 - Carregando resultado de filmes favoritos

--- app/src/main/java/matheusfelipe/desafio/inchurch/presentation/pages/favorite_movies/FavoriteMoviesActivity.kt
+++ app/src/main/java/matheusfelipe/desafio/inchurch/presentation/pages/favorite_movies/FavoriteMoviesActivity.kt
@@ -1,12 +1,73 @@
 package matheusfelipe.desafio.inchurch.presentation.pages.favorite_movies
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.View
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import matheusfelipe.desafio.inchurch.core.utils.Response
+import matheusfelipe.desafio.inchurch.core.utils.Status
 
 class FavoriteMoviesActivity: AppCompatActivity() {
+
+    private lateinit var viewModel: FavoriteMoviesViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel = ViewModelProviders.of(this)[FavoriteMoviesViewModel::class.java]
+        viewModel.movies().observe(this, Observer { response -> processResponse(response) })
+    }
+
+    private fun processResponse(response: Response){
+        when(response.status){
+            Status.LOADING -> showLoading()
+            Status.SUCCESS -> showMovies(response.data)
+            Status.EMPTY_RESPONSE -> {}
+            Status.ERROR -> showError(response.error)
+        }
+    }
+
+    private fun showLoading(){
+        //pg_loading.visibility = View.VISIBLE
+    }
+
+    private fun showMovies(data: Any?){
+
+//        pg_loading.visibility = View.GONE
+//
+        if(data is List<*>) {
+            Log.i("script2", data.size.toString())
+        }
+//
+//            movieAdapter = MovieAdapter(data.filterIsInstance<Movie>().toMutableList(), ::onItemClick, ::onFavoriteClick)
+//
+//            val layoutManager = GridLayoutManager(this, 2)
+//            rv_movies.layoutManager = layoutManager
+//            rv_movies.adapter = movieAdapter
+//        }
+    }
+
+    private fun showError(throwable: Throwable?){
+//        pg_loading.visibility = View.GONE
+//        tv_error.text = throwable?.message
+//        tv_error.visibility = View.VISIBLE
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        //menuInflater.inflate(R.menu.menu_pedido_visualizacao, menu)
+
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/matheusfelipe/desafio/inchurch/presentation/pages/favorite_movies/FavoriteMoviesViewModel.kt
+++ app/src/main/java/matheusfelipe/desafio/inchurch/presentation/pages/favorite_movies/FavoriteMoviesViewModel.kt
@@ -1,14 +1,54 @@
 package matheusfelipe.desafio.inchurch.presentation.pages.favorite_movies
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import matheusfelipe.desafio.inchurch.core.api.MovieApi
+import matheusfelipe.desafio.inchurch.core.api.RestApi
+import matheusfelipe.desafio.inchurch.core.utils.Response
+import matheusfelipe.desafio.inchurch.data.data_sources.MovieLocalDataSource
+import matheusfelipe.desafio.inchurch.data.data_sources.MovieLocalDataSourceImpl
+import matheusfelipe.desafio.inchurch.data.data_sources.MovieRemoteDataSource
+import matheusfelipe.desafio.inchurch.data.data_sources.MovieRemoteDataSourceImpl
+import matheusfelipe.desafio.inchurch.data.repositories.MovieRepositoryImpl
+import matheusfelipe.desafio.inchurch.domain.repositories.MovieRepository
+import matheusfelipe.desafio.inchurch.domain.usecases.GetFavoriteMovies
+import matheusfelipe.desafio.inchurch.domain.usecases.SelectDetailMovie
 
 class FavoriteMoviesViewModel: ViewModel() {
 
-//    private lateinit var getAllMovies: GetAllMovies
-//    private lateinit var movieRepository: MovieRepository
-//    private lateinit var movieRemoteDataSource: MovieRemoteDataSource
-//    private lateinit var movieLocalDataSource: MovieLocalDataSource
-//
-//    private var movies: MutableLiveData<Response> = MutableLiveData()
-//    fun movies() = movies
+    private lateinit var movieRemoteDataSource: MovieRemoteDataSource
+    private lateinit var movieLocalDataSource: MovieLocalDataSource
+    private lateinit var movieRepository: MovieRepository
+    private lateinit var getFavoriteMoviesUseCase: GetFavoriteMovies
+    private lateinit var selectDetailMovieUseCase: SelectDetailMovie
+
+    private var movies: MutableLiveData<Response> = MutableLiveData()
+    fun movies() = movies
+
+    init {
+        val api = RestApi.getRetrofit().create(MovieApi::class.java)
+        movieRemoteDataSource = MovieRemoteDataSourceImpl(api)
+        movieLocalDataSource = MovieLocalDataSourceImpl()
+        movieRepository = MovieRepositoryImpl(movieRemoteDataSource, movieLocalDataSource)
+        getFavoriteMoviesUseCase = GetFavoriteMovies(movieRepository)
+        selectDetailMovieUseCase = SelectDetailMovie(movieRepository)
+
+        fetchFavoriteMovies()
+    }
+
+    private fun fetchFavoriteMovies() {
+
+        movies.postValue(Response.loading())
+        CoroutineScope(Dispatchers.IO).launch {
+            try{
+                val result = getFavoriteMoviesUseCase()
+                movies.postValue(Response.success(result))
+            } catch (t: Throwable) {
+                movies.postValue(Response.error(t))
+            }
+        }
+    }
 }
