d1a355b03b8d3678312a84f30a636758bc1da7f9
Felcks <matheusfelipecorreaalves@gmail.com>
2020-04-19 17:45:22 -0300

    Injeção de dependencia 02 - Injeção em MoviesActivity completa

--- app/src/main/java/matheusfelipe/desafio/inchurch/core/DependencyModules.kt
+++ app/src/main/java/matheusfelipe/desafio/inchurch/core/DependencyModules.kt
@@ -2,6 +2,14 @@ package matheusfelipe.desafio.inchurch.core
 
 import matheusfelipe.desafio.inchurch.core.api.MovieApi
 import matheusfelipe.desafio.inchurch.core.api.RestApi
+import matheusfelipe.desafio.inchurch.data.data_sources.MovieLocalDataSource
+import matheusfelipe.desafio.inchurch.data.data_sources.MovieLocalDataSourceImpl
+import matheusfelipe.desafio.inchurch.data.data_sources.MovieRemoteDataSource
+import matheusfelipe.desafio.inchurch.data.data_sources.MovieRemoteDataSourceImpl
+import matheusfelipe.desafio.inchurch.data.repositories.MovieRepositoryImpl
+import matheusfelipe.desafio.inchurch.domain.repositories.MovieRepository
+import matheusfelipe.desafio.inchurch.domain.usecases.*
+import matheusfelipe.desafio.inchurch.presentation.pages.favorite_movies.FavoriteMoviesViewModel
 import matheusfelipe.desafio.inchurch.presentation.pages.movies.MoviesViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -11,7 +19,20 @@ object DependencyModules {
     val appModule = module{
 
         single<MovieApi> {  RestApi.getRetrofit().create(MovieApi::class.java) }
+        single<MovieLocalDataSource> { MovieLocalDataSourceImpl() }
+        single<MovieRemoteDataSource> { MovieRemoteDataSourceImpl(get<MovieApi>()) }
 
-        viewModel<MoviesViewModel> { MoviesViewModel()}
+        single<MovieRepository> { MovieRepositoryImpl(get(), get())}
+
+        factory<FavoriteOrDisfavorMovie> { FavoriteOrDisfavorMovie(get()) }
+        factory<GetAllMovies> { GetAllMovies(get())}
+        factory<GetAllMoviesGenres> { GetAllMoviesGenres(get())}
+        factory<GetFavoriteMovies> { GetFavoriteMovies(get())}
+        factory<SelectDetailMovie> { SelectDetailMovie(get())}
+        factory<ViewDetailMovie> { ViewDetailMovie(get())}
+
+
+        viewModel<MoviesViewModel> { MoviesViewModel(get(), get(), get(), get()) }
+
     }
 }
--- app/src/main/java/matheusfelipe/desafio/inchurch/presentation/pages/movies/MoviesViewModel.kt
+++ app/src/main/java/matheusfelipe/desafio/inchurch/presentation/pages/movies/MoviesViewModel.kt
@@ -5,31 +5,19 @@ import androidx.lifecycle.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import matheusfelipe.desafio.inchurch.core.api.MovieApi
-import matheusfelipe.desafio.inchurch.core.api.RestApi
 import matheusfelipe.desafio.inchurch.core.utils.Response
-import matheusfelipe.desafio.inchurch.data.data_sources.MovieLocalDataSource
-import matheusfelipe.desafio.inchurch.data.data_sources.MovieLocalDataSourceImpl
-import matheusfelipe.desafio.inchurch.data.data_sources.MovieRemoteDataSource
-import matheusfelipe.desafio.inchurch.data.data_sources.MovieRemoteDataSourceImpl
-import matheusfelipe.desafio.inchurch.data.repositories.MovieRepositoryImpl
 import matheusfelipe.desafio.inchurch.domain.entities.Movie
-import matheusfelipe.desafio.inchurch.domain.repositories.MovieRepository
 import matheusfelipe.desafio.inchurch.domain.usecases.FavoriteOrDisfavorMovie
 import matheusfelipe.desafio.inchurch.domain.usecases.GetAllMovies
 import matheusfelipe.desafio.inchurch.domain.usecases.GetFavoriteMovies
 import matheusfelipe.desafio.inchurch.domain.usecases.SelectDetailMovie
 
-class MoviesViewModel : ViewModel() {
-
-
-    private lateinit var favoriteOrDisfavorMovieUseCase: FavoriteOrDisfavorMovie
-    private lateinit var selectDetailMovieUseCase: SelectDetailMovie
-    private lateinit var getAllMovies: GetAllMovies
-    private lateinit var getFavoriteMoviesUseCase: GetFavoriteMovies
-    private lateinit var movieRepository: MovieRepository
-    private lateinit var movieRemoteDataSource: MovieRemoteDataSource
-    private lateinit var movieLocalDataSource: MovieLocalDataSource
+class MoviesViewModel(
+    val favoriteOrDisfavorMovieUseCase: FavoriteOrDisfavorMovie,
+    val selectDetailMovieUseCase: SelectDetailMovie,
+    val getAllMovies: GetAllMovies,
+    val getFavoriteMoviesUseCase: GetFavoriteMovies
+) : ViewModel() {
 
     private var movies: MutableLiveData<Response> = MutableLiveData()
     fun movies() = movies
@@ -46,15 +34,6 @@ class MoviesViewModel : ViewModel() {
     private var currentPage = 1
 
     init {
-        val api = RestApi.getRetrofit().create(MovieApi::class.java)
-        movieRemoteDataSource = MovieRemoteDataSourceImpl(api)
-        movieLocalDataSource = MovieLocalDataSourceImpl()
-        movieRepository = MovieRepositoryImpl(movieRemoteDataSource, movieLocalDataSource)
-        getAllMovies = GetAllMovies(movieRepository)
-        selectDetailMovieUseCase = SelectDetailMovie(movieRepository)
-        favoriteOrDisfavorMovieUseCase = FavoriteOrDisfavorMovie(movieRepository)
-        getFavoriteMoviesUseCase = GetFavoriteMovies(movieRepository)
-
         fetchAllMovies()
     }
 
